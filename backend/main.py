265774ccddaf557dc891d0762163f89429791e69
carlosmab <carlosm.araujob@gmail.com>
2022-11-17 11:24:11 -0500

    Database methods and models configured

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 # App object
 app = FastAPI()
+
+from database import (
+    fetch_one_todo,
+    fetch_all_todos,
+    create_todo,
+    update_todo,
+    remove_todo,
+    Todo                      
+)
 
 origins = ['http://localhost:3000']
 
@@ -20,20 +29,33 @@ def read_root():
 
 @app.get("/api/todo")
 async def get_todo():
-    return 1
+    response = await fetch_all_todos()
+    return response
 
-@app.get("/api/todo{id}")
-async def get_todo_by_id(id):
-    return 1
+@app.get("/api/todo{title}")
+async def get_todo_by_id(title):
+    response = await fetch_one_todo(title)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no todo with title {title}")
 
-@app.post("/api/todo")
-async def post_todo_by_id():
-    return 1
+@app.post("/api/todo", response_model=Todo)
+async def post_todo_by_id(todo:Todo):
+    response = await create_todo(todo.dict())
+    if response:
+        return response
+    raise HTTPException(400, f"Something went wrong / Bad Request")
 
-@app.put("/api/todo{id}")
-async def put_todo(id, data):
-    return 1
+@app.put("/api/todo{title}", response_model=Todo)
+async def put_todo(title:str, desc:str):
+    response = await update_todo(title, desc)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no todo with title {title}")
 
-@app.delete("/api/todo{id}")
-async def delete_todo(id):
-    return 1
+@app.delete("/api/todo{title}")
+async def delete_todo(title):
+    response = await remove_todo(title)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no todo with title {title}") 
